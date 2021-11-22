fe70038ad861b38fb8ce2835f9e37fbe21d58bd0
RafaeLovera6 <rafaelovera6@gmail.com>
2021-11-22 23:16:52 +0000

    excusegenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let who1 = who[Math.floor(Math.random() * who.length)];
+  let action2 = action[Math.floor(Math.random() * action.length)];
+  let what3 = what[Math.floor(Math.random() * what.length)];
+  let when4 = when[Math.floor(Math.random() * when.length)];
+
+  document.querySelector("#excuse").innerHTML =
+    who1 + " " + action2 + " " + what3 + " " + when4;
 };
+
+console.log("Hello my name is rigo!");
