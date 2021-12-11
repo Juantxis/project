c2add19669971904fc89a7e0bf1387c22a0cbc98
Luis Piñero Quintana <lpineroq@gmail.com>
2021-12-11 12:07:31 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,18 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let pronouns = ["the", "one", "my", "your", "their", "some"];
+let adjectives = ["stupid", "new", "old", "technical", "awfull", "good"];
+let objects = ["idea", "web", "nosense", "project", "program", "thing"];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < objects.length; k++) {
+        document.write(
+          pronouns[i].concat(adjectives[j], objects[k], ".com" + "<br>")
+        );
+      }
+    }
+  }
 };
