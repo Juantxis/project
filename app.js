239b974abc4034cfe5efb134c77bc4cc5ecbd711
andreshalac <andreshalac@gmail.com>
2022-05-26 19:27:40 +0000

    se creo fichero app.js en la raiz

---
+++ app.js
@@ -0,0 +1,10 @@
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let z = 0; z < adj.length; z++)
+    for (let x = 0; x < noun.length; x++) {
+      console.log(pronoun[i], adj[z], noun[x] + ".com");
+    }
+}
