3a65fcc12266c562ffc52eb619558489286375d1
dariel0514 <dariel0514@hotmail.com>
2022-08-28 16:53:19 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let p = document.querySelector("#excuse");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoRandomNumber = Math.floor(Math.random() * who.length);
+  let actionRandomNumber = Math.floor(Math.random() * action.length);
+  let whatRandomNumber = Math.floor(Math.random() * what.length);
+  let whenRandomNumber = Math.floor(Math.random() * when.length);
+
+  p.innerHTML = `${who[whoRandomNumber]} ${action[actionRandomNumber]} ${what[whatRandomNumber]} ${when[whenRandomNumber]}
+  `;
 };
