452b1de433a69312135ec27e550dbf19e1b0ea75
donpablohdf <pabloalegrejuan@gmail.com>
2022-09-30 01:39:10 +0000

    Hacer función de generar carta aleatoria

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
+var el = document.getElementById("generaCarta");
+el.addEventListener("click", crearCarta, false);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function crearCarta(event) {
+  console.clear();
+  const NUMERO = document.getElementById("numero");
+  const PALO_ARRIBA = document.getElementById("paloArriba");
+  const PALO_ABAJO = document.getElementById("paloAbajo");
+  //Genera el número aleatorio
+  let losNumeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let eligeNumero = Math.floor(Math.random() * losNumeros.length);
+  NUMERO.innerHTML = losNumeros[eligeNumero];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let palos = [
+    '<img src="assets/img/treboles.png">',
+    '<img src="assets/img/picas.png">',
+    '<img src="assets/img/corazon.png">',
+    '<img src="assets/img/diamante.png">'
+  ];
+  let eligePalo = Math.floor(Math.random() * palos.length);
+
+  PALO_ARRIBA.innerHTML = palos[eligePalo];
+  PALO_ABAJO.innerHTML = palos[eligePalo];
+  // si los palos son diamantes o corazones cambia el color a rojo
+  if (palos[eligePalo] === "♥" || palos[eligePalo] === "♦") {
+    PALO_ARRIBA.style.color = "red";
+    PALO_ABAJO.style.color = "red";
+    NUMERO.style.color = "red";
+  }
+}
