b6a982cceae1da20ace022ec4d75fc3b7ae37640
Àlex Rofes Galobardes <129336104+RofinhoOo@users.noreply.github.com>
2023-05-02 19:29:49 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+
+
+const excuseElement = document.querySelector("#excuse");
+
+  
+  let who = ['The dog','My grandma','His turtle','My bird'];
+  let action = ['ate','peed','crushed','broke'];
+  let what = ['my homework', 'the keys', 'the car'];
+  let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+ 
+  function generarExcusa() {
+    let whoAleatorio = who[Math.floor(Math.random() * who.length)];
+    let actionAleatorio = action[Math.floor(Math.random() * action.length)];
+    let whatAleatorio = what[Math.floor(Math.random() * what.length)];
+    let whenAleatorio = when[Math.floor(Math.random() * when.length)];
+
+    // Crear la excusa aleatoria
+
+    let excusa = whoAleatorio + " " + actionAleatorio + " " + whatAleatorio + " " + whenAleatorio + ".";
+
+    // Establecer la excusa en el elemento HTML con ID #excuse
+
+    document.getElementById("excuse").innerHTML = excusa;
+    const mainParagraph = document.getElementById('excuse');
+
+  console.log(mainParagraph);
+  }
+
+  // Llamar a la función generarExcusa cuando se carga la página
+
+  generarExcusa();
+
