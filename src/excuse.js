804871a3db1deafd16d5600a42b66f26e60f838c
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-05-11 01:08:49 +0000

    first xcuse generator

---
+++ src/excuse.js
@@ -0,0 +1,17 @@
+function generateExcuse() {
+    let who = ['The dog','My grandma','His turtle','My bird'];
+    let action = ['ate','peed','crushed','broke'];
+    let what = ['my homework', 'the keys', 'the car'];
+    let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+    
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+    
+    let excuse = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+    document.getElementById("excuse").innerHTML = excuse;
+
+}
+
+generateExcuse();
