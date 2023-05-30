8a1b27b6781c52e6edb78f300614f52acd9d3c9c
Salomon Franco <salomonfrancodasilva@gmail.com>
2023-05-30 02:09:25 +0100

    scriptCreated&DesignEnhanced
    The javascript file was updated to generate after the click a button the script

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const somebody = ["luke", "vader", "leia", "han"];
+  const action = ["fought", "saved", "beat", "found"];
+  const whatever = ["star", "force", "saber", "droid"];
+  const when = ["wars", "rogue", "clones", "jedi"];
+  const domainEnd = [".com", ".es", ".net", ".org"];
+
+  const somebodyStarWars = Math.floor(Math.random() * somebody.length);
+  const actionStarWars = Math.floor(Math.random() * action.length);
+  const whateverStarWars = Math.floor(Math.random() * whatever.length);
+  const whenStarWars = Math.floor(Math.random() * when.length);
+  const domainEndStarWars = Math.floor(Math.random() * domainEnd.length);
+
+  const domainStarWars =
+    somebody[somebodyStarWars] +
+    action[actionStarWars] +
+    whatever[whateverStarWars] +
+    when[whenStarWars] +
+    domainEnd[domainEndStarWars];
+  document.getElementById("domainGenerator").textContent = domainStarWars;
 };
