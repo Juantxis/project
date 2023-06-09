48a0c73b7a68e0ee7912652b1dc284429e992391
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-06-09 11:09:31 -0400

    added two arrays card suits & values

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./bootstrap/dist/js/bootstrap.min.js";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const cardSuits = ['hearts', 'spades', 'clubs', 'diamonds'];
+  const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'King', 'Queen', 'Jack', 'Ace'];
+  
+  
+  };
+  console.log("Hello from the console!");
