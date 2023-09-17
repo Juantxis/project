94baf90bfb116b48095a74ea00fa47916e4cb7e1
Jorge Oteiza <oteiza.jor@gmail.com>
2023-09-17 22:41:07 +0000

    1er intento excuse

--- src/app.js
+++ src/app.js
@@ -9,3 +9,14 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
