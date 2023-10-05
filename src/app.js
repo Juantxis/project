efe763c1be6327fc83e187742ecd5268a1e93fe8
AndyGH86 <141663667+AndyGH86@users.noreply.github.com>
2023-10-05 16:39:06 +0200

    cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector('#excusegenerate').addEventListener("click", () => {
+    document.querySelector('#excuse').innerHTML = generateExcuse();
+  });
+  console.log("Hola Hans");
 };
+  
+  const who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my homework', 'the keys', 'the car'];
+  const when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+  let generateExcuse = () => {
+  
+  const indexwho = Math.floor(Math.random() * who.length)
+  const indexaction = Math.floor(Math.random() * action.length)
+  const indexwhat = Math.floor(Math.random() * what.length)
+  const indexwhen = Math.floor(Math.random() * when.length)
+  
+  return who[indexwho] +" "+ action[indexaction] +" " + what[indexwhat] + " " + when[indexwhen];
+};
+
+
