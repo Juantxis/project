3ebdb792bac01f26233f77f40220b1421bedbbf9
Pedro Aguirregomezcorta <paguirre2391@gmail.com>
2023-11-19 23:30:20 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
+
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector('#btn'). addEventListener("click",() =>{
+    document.querySelector("#the-excuse").innerHTML = gExcuses();
+  });
+  console.log("Hello Rigo from the console! ");
+};
+let gExcuses = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let whoIndex = Math.flow(Math.random()*who.length);
+  let actionIndex = Math.flow(Math.random()*action.length);
+  let whatIndex = Math.flow(Math.random()*what.length);
+  let whenIndex = Math.flow(Math.random()*when.length);
+  
+    return who[whoIndex] + ''+ action[actionIndex] + ' '+what[whatIndex]+''+when[whenIndex];
 };
