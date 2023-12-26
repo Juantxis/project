46f7fd37686804fd6cf25cf2aadb520d06238bc2
Pedro Aguirregomezcorta <paguirre2391@gmail.com>
2023-12-26 12:56:08 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,16 +8,3 @@ window.onload = () => {
   });
   console.log("Hello Rigo from the console! ");
 };
-let gExcuses = () => {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
-
-  let whoIndex = Math.flow(Math.random()*who.length);
-  let actionIndex = Math.flow(Math.random()*action.length);
-  let whatIndex = Math.flow(Math.random()*what.length);
-  let whenIndex = Math.flow(Math.random()*when.length);
-  
-    return who[whoIndex] + ''+ action[actionIndex] + ' '+what[whatIndex]+''+when[whenIndex];
-};
