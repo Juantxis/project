74a8a5c27b917c856b0697fdbe2f418f81cf64f2
Pedro Aguirregomezcorta <paguirre2391@gmail.com>
2023-12-27 14:58:34 -0300

    Create GeneradorNombres.js
    Se subio el proyecto Generador de nombres

---
+++ src/GeneradorNombres.js
@@ -0,0 +1,24 @@
+function generarPaginas() {
+  for (let index = 0; index < 10; index++) {
+    let pronoun = ['the', 'our', 'tha'];
+    let adj = ['great', 'big', 'small'];
+    let noun = ['jogger', 'racoon', 'bird'];
+    let DNS = ['.com', '.net', '.cl'];
+
+    const indiceAleatorio = Math.floor(Math.random() * pronoun.length);
+    const indiceAleatorio1 = Math.floor(Math.random() * adj.length);
+    const indiceAleatorio2 = Math.floor(Math.random() * noun.length);
+    const indiceAleatorio3 = Math.floor(Math.random() * DNS.length);
+
+    const elementoAleatorio = pronoun[indiceAleatorio];
+    const elementoAleatorio1 = adj[indiceAleatorio1];
+    const elementoAleatorio2 = noun[indiceAleatorio2];
+    const elementoAleatorio3 = DNS[indiceAleatorio3];
+
+    const excusaGenerada = elementoAleatorio + elementoAleatorio1 + elementoAleatorio2 + elementoAleatorio3;
+    console.log(excusaGenerada);
+  }
+}
+
+generarPaginas();
+
