44989981cf498033d372491b20fecb245655e102
Matteo0310 <145265011+Matteo0310@users.noreply.github.com>
2023-12-31 15:31:04 +0000

    feat: Card Generator acabado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function newCard() {
+  let suit = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "k",
+    "Q"
+  ];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  let top = document.querySelector("#top");
+  let end = document.querySelector("#end");
+  let number = document.querySelector("#number");
+
+  top.innerHTML = suit[indexSuit];
+  end.innerHTML = suit[indexSuit];
+  number.innerHTML = numbers[indexNumbers];
+
+  let currentClass =
+    suit[indexSuit] == "♦" || suit[indexSuit] == "♥"
+      ? "text-danger"
+      : "text-dark";
+  top.className = currentClass;
+  end.className = currentClass;
+  number.className = currentClass;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  newCard();
 };
+
+let button = document.querySelector("#button");
+button.addEventListener("click", () => newCard());
