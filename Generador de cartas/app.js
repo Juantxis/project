fe1ceca2cc6de60675effad930536f232b8780d4
Pedro Aguirregomezcorta <paguirre2391@gmail.com>
2024-01-02 16:16:12 -0300

    Update app.js

--- Generador de cartas/app.js
+++ Generador de cartas/app.js
@@ -1,7 +1,8 @@
-// Variables
+
 const simbolos = ['♠', '♥', '♦', '♣'];
 const numeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
+//Esta funcion elige de manera aleatoria numeros, simbolos y colores de las cartas
 function generarCarta() {
   const aSimbolos = simbolos[Math.floor(Math.random() * simbolos.length)];
   const aNumero = numeros[Math.floor(Math.random() * numeros.length)];
@@ -21,6 +22,7 @@ function generarCarta() {
   numeroE.textContent = aNumero;
   numeroE.style.color = color;
 
+  //De esta manera logre generar el segundo simbolo
   const simbolo2E = document.getElementById('simbolo2');
   simbolo2E.textContent = aSimbolos; 
   simbolo2E.style.color = color;
