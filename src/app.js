9eb5b178691eaa7bfac2d4fa144aa6e42a156329
Pedro Aguirregomezcorta <paguirre2391@gmail.com>
2024-01-02 15:05:06 -0300

    Update app.js
    cambie el idioma de las variables y utilice $

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
+//Ademas de cambiar el codigo de archivo, cambie las variables a un solo idioma
+function excusesGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = ["before the class", "right on time", "when I finished", "during my lunch", "while I was praying"];
 
-import "bootstrap";
-import "./style.css";
+  const indexRandom = Math.floor(Math.random() * who.length);
+  const indexRandom1 = Math.floor(Math.random() * action.length);
+  const indexRandom2 = Math.floor(Math.random() * what.length);
+  const indexRandom3 = Math.floor(Math.random() * when.length);
 
-window.onload = () => {
-  document.querySelector('#btn'). addEventListener("click",() =>{
-    document.querySelector("#the-excuse").innerHTML = gExcuses();
-  });
-  console.log("Hello Rigo from the console! ");
-};
+  const randomElement = who[indexRandom];
+  const randomElement1 = action[indexRandom1];
+  const randomElement2 = what[indexRandom2];
+  const randomElement3 = when[indexRandom3];
+
+  // Cambie la variable excuses con $
+
+  const excuses = `${randomElement} ${randomElement1} ${randomElement2} ${randomElement3}`;
+
+
+  const myDiv = document.getElementById("myText");
+  myDiv.innerText = excuses;
+
+  myDiv.style.textAlign = "center";
+}
+
+excusesGenerator();
