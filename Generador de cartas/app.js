246b0ece08c71e3f3faf0ee8d41d752182b3b0e7
Pedro <pedro@MacBook-Pro-de-Pedro.local>
2024-01-02 16:09:02 -0300

    Se hicieron los cambios correspondientes

---
+++ Generador de cartas/app.js
@@ -0,0 +1,27 @@
+// Variables
+const simbolos = ['♠', '♥', '♦', '♣'];
+const numeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+function generarCarta() {
+  const aSimbolos = simbolos[Math.floor(Math.random() * simbolos.length)];
+  const aNumero = numeros[Math.floor(Math.random() * numeros.length)];
+  let color = '';
+
+  if (aSimbolos === '♠' || aSimbolos === '♣') {
+    color = 'black';
+  } else {
+    color = 'red';
+  }
+  
+  const simbolo1E = document.getElementById('simbolo1');
+  simbolo1E.textContent = aSimbolos;
+  simbolo1E.style.color = color;
+
+  const numeroE = document.getElementById('numero');
+  numeroE.textContent = aNumero;
+  numeroE.style.color = color;
+
+  const simbolo2E = document.getElementById('simbolo2');
+  simbolo2E.textContent = aSimbolos; 
+  simbolo2E.style.color = color;
+}
