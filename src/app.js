17d7c0a0659995828b223f45c3cecb284aadfc0e
Karel-CR <52536063+Karel-CR@users.noreply.github.com>
2024-01-10 20:11:14 +0000

    Main

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 /* eslint-disable */
+/*
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+*/
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //declaramos arrays
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
+  //Declaramos una función que se encargará de crear un  array con numeros aleatorios.
+  function generateRandomNumbers(who, action, what, when) {
+    let randomWho = parseInt(Math.random() * who.length);
+    let randomAction = parseInt(Math.random() * action.length);
+    let randomWhat = parseInt(Math.random() * what.length);
+    let randomWhen = parseInt(Math.random() * when.length);
+    return [randomWho, randomAction, randomWhat, randomWhen];
+  }
+  //Declaramos un Array para contener los números aleatorios
+  let arrayAleatoryNumbers = generateRandomNumbers(who, action, what, when);
+  // Concatenamos cadenas
+  let myText = who[arrayAleatoryNumbers[0]]+" "+action[arrayAleatoryNumbers[1]]+" "+what[arrayAleatoryNumbers[2]]+" "+when[arrayAleatoryNumbers[3]];
+   // Imprimimos en el index
+  document.getElementById("excuse").innerHTML = myText;
 };
