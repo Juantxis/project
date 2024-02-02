9eedf16be6ad1c353bf86fd129f7494181d34c63
muldersgf69 <109549032+muldersgf69@users.noreply.github.com>
2024-02-02 07:30:58 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function() {};
+const whoArray = [];
+("");
