da40685c196037c593ea5a63036cfd5c09892154
muldersgf69 <109549032+muldersgf69@users.noreply.github.com>
2024-02-02 22:19:04 +0000

    second commit

--- src/excuse.js
+++ src/excuse.js
@@ -6,22 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  const initialExcuse = "My dog ate my homework";
-  document.getElementById("excuse").innerText = initialExcuse;
+  const whoArray = ['My dog', 'My granny', 'My mom'];
+  const whatArray = ['ate', 'burned', 'ripped'];
+  const objectArray = ['my homework', 'the keys', 'the car'];
+  const whenArray = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
 
-  const generateNewExcuse = () => {
-    const whoArray = ["my dog", "my granny", "my mom"];
-    const whatArray = ["ate", "burned", "ripped"];
-    const homeWork = ["my homework"];
-
+  // Generate a random sentence
+  const generateRandomSentence = () => {
     const whoIndex = Math.floor(Math.random() * whoArray.length);
     const whatIndex = Math.floor(Math.random() * whatArray.length);
-    const homeWorkIndex = Math.floor(Math.random() * homeWork.length);
+    const objectIndex = Math.floor(Math.random() * objectArray.length);
+    const whenIndex = Math.floor(Math.random() * whenArray.length);
 
-    const newExcuse = `${whoArray[whoIndex]} ${whatArray[whatIndex]} ${homeWork[homeWorkIndex]}.`;
-
-    return newExcuse; // Return the new excuse string
+    const randomSentence = `${whoArray[whoIndex]} ${whatArray[whatIndex]} ${objectArray[objectIndex]} ${whenArray[whenIndex]}.`;
+    return randomSentence;
   };
+
+  // Get the element where the random sentence will be displayed
+  const randomSentenceElement = document.getElementById('randomSentence');
+
+  // Generate and display a random sentence when the page loads
+  randomSentenceElement.innerText = generateRandomSentence();
+};
+
 
   const newExcuseButton = document.getElementById("newExcuse");
   newExcuseButton.addEventListener("click", function() {
