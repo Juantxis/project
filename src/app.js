7232c638b3db9762a7734f89d207f94ec9ca1493
Juantxis <154518249+Juantxis@users.noreply.github.com>
2024-05-14 10:56:58 +0000

    projectinstagram

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generatexcuse();
+  console.log("Hello Rigo from the console");
+};
+let generatexcuse = () => {
+  let who = ["un gorila", "un loro", "mi abuela", "Spiderman"];
+  let action = [" pintó ", " se comió ", " tiró ", " rompió "];
+  let what = [" mis deberes ", " mi ordenador ", " mi móvil "];
+  let when = [
+    " mientras dormía ",
+    " mientras jugaba al fútbol ",
+    " mientras estaba surfeando "
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    "" +
+    action[actionIndex] +
+    "" +
+    what[whatIndex] +
+    "" +
+    when[whenIndex] +
+    ""
+  );
 };
